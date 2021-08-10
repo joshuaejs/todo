24f7be64de0f049b15dedaa7b79fa834da434017
jejs <github@sahala.org>
2021-08-09 19:32:42 -0700

    added del option

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -45,6 +45,7 @@ func main() {
 	add := flag.Bool("add", false, "Add task to the ToDo list")
 	list := flag.Bool("list", false, "List all tasks")
 	complete := flag.Int("complete", 0, "Item number to be completed")
+    del := flag.Int("del", 0, "Item number to be deleted")
 
 	flag.Parse()
 
@@ -86,6 +87,16 @@ func main() {
 		l.Add(t)
 
 		// Save the new list
+		if err := l.Save(todoFileName); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+    case *del > 0:
+        // Delete the given task
+        if err := l.Delete(*del); err != nil {
+            fmt.Fprintln(os.Stderr, err)
+            os.Exit(1)
+        }
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
